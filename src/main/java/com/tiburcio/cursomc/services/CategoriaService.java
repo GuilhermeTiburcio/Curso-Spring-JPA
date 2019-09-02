92ca44fce0ca92676ad79eeafc2d979ac35191f4
Guilherme Tiburcio do Nascimento - GNA <guilherme.nascimento@iteris.local>
2019-09-02 16:40:54 -0300

    Valid NotEmpyti

--- src/main/java/com/tiburcio/cursomc/services/CategoriaService.java
+++ src/main/java/com/tiburcio/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.tiburcio.cursomc.domain.Categoria;
+import com.tiburcio.cursomc.dto.CategoriaDTO;
 import com.tiburcio.cursomc.repositories.CategoriaRepository;
 import com.tiburcio.cursomc.services.exceptions.DataIntegrityException;
 import com.tiburcio.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
