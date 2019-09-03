0738f1ceb617c21c6c36228ee84ed45f3df09418
Guilherme Tiburcio do Nascimento - GNA <guilherme.nascimento@iteris.local>
2019-09-03 11:17:07 -0300

    Adicionando consulta produtos

--- src/main/java/com/tiburcio/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/tiburcio/cursomc/dto/ClienteDTO.java
@@ -8,10 +8,12 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.tiburcio.cursomc.domain.Cliente;
+import com.tiburcio.cursomc.services.validation.ClienteUpdate;
 
 import lombok.Data;
 
 @Data
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
